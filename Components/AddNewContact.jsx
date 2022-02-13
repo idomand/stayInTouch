4107743cfe2a0bd6731a20fc6c607711e187be47
Ido Mandelman <idomand@gmail.com>
2022-02-13 18:27:42 +0100

    new AddNewContact- basic sturcture- done

--- Components/AddNewContact.jsx
+++ Components/AddNewContact.jsx
@@ -4,58 +4,78 @@ import "react-datepicker/dist/react-datepicker.css";
 import { addContactToFirestore } from "../lib/Firebase";
 import { useAuth } from "../lib/AuthContext";
 import ErrorWrapper from "./ErrorWarning";
-import { P } from "./Common/StyledText";
 import { BasicForm, BasicLabel } from "./Common/StyledFormElements";
 import { BasicInput, InputSubmit } from "./Common/StyledFormElements";
 import DatePickerComponent from "./DatePickerComponent";
+
 const AddContactForm = styled(BasicForm)`
   display: grid;
   padding: 15px;
+  gap: 5px;
   grid-template-areas:
     "name howMuchTime tag"
-    "lastTalked notes"
-    "submit";
-  width: 50vw;
+    "lastTalked . notes"
+    "submit submit submit";
+
+  width: 50%;
   @media (${({ theme }) => theme.devices.break1}) {
-    width: 90vw;
+    width: 90%;
     grid-template-areas:
       "name howMuchTime "
       "tag lastTalked "
-      "notes"
-      "submit";
+      "notes notes"
+      "submit submit";
   }
 `;
 
-// const InputText = styled(BasicInput)``;
-// const InputTime = styled(BasicInput)``;
-
 const NameLabel = styled(BasicLabel)`
+  grid-area: name;
   border: dotted blue;
 `;
 const NameInput = styled(BasicInput)`
   border: solid blue;
 `;
-// const TagLabel = styled(BasicLabel)`
-// border: dotted green;
-// `;
-// const TagInput = styled(BasicInput)`
-//   border: solid green;
-// `;
-// const TimeLabel = styled(BasicLabel)`
-//   border: dotted yellow;
-// `;
+const TimeLabel = styled(BasicLabel)`
+  grid-area: howMuchTime;
+
+  border: dotted yellow;
+`;
 
 const TimeInput = styled(BasicInput)`
   border: solid yellow;
 `;
+const TagLabel = styled(BasicLabel)`
+  grid-area: tag;
+  border: dotted green;
+`;
+// const TagInput = styled(BasicInput)`
+//   border: solid green;
+// `;
+const TagInput = styled.select`
+  border: solid green;
+`;
+const TagOption = styled.option`
+  background-color: goldenrod;
+  border: blue solid;
+`;
 
-// const NotesLabel = styled(BasicLabel)`
-//   border: dotted orange;
+const LastTalkedLabel = styled(BasicLabel)`
+  grid-area: lastTalked;
+  border: dotted black;
+`;
+
+// const lastTalkedInput = styled(BasicInput)`
+//   border: solid black;
 // `;
 
-// const NotesInput = styled(BasicInput)`
-//   border: solid orange;
-// `;
+const NotesLabel = styled(BasicLabel)`
+  grid-area: notes;
+  border: dotted orange;
+`;
+
+const NotesInput = styled.textarea`
+  border: solid orange;
+`;
 
 const AddSubmitInput = styled(InputSubmit)`
   grid-area: submit;
@@ -69,13 +89,6 @@ const AddSubmitInput = styled(InputSubmit)`
     color: ${({ theme }) => theme.green_1};
   }
 `;
-const DatePickerWrapper = styled.div`
-  grid-area: lastTalked;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
 
 export default function AddNewContact() {
   const timeRef = useRef();
@@ -83,6 +96,8 @@ export default function AddNewContact() {
   const [name, setName] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [error, setError] = useState(false);
+  const [tagValue, setTagValue] = useState("");
+  const [note, setNote] = useState("");
 
   function nameChangeHandler(e) {
     setName(e.target.value);
@@ -126,8 +141,8 @@ export default function AddNewContact() {
             placeholder="Enter name"
           />
         </NameLabel>
-        <BasicLabel>
-          every
+        <TimeLabel>
+          Every
           <TimeInput
             ref={timeRef}
             type="number"
@@ -137,20 +152,45 @@ export default function AddNewContact() {
             min={1}
             defaultValue={3}
           />
-          days
-        </BasicLabel>
-        <DatePickerWrapper>
-          <P>Last time we have talked was:</P>
+        </TimeLabel>
+        <TagLabel>
+          Add a Tag:
+          <TagInput
+            onChange={(e) => {
+              setTagValue(e.target.value);
+            }}
+          >
+            <TagOption>select</TagOption>
+            <TagOption>Coworker</TagOption>
+            <TagOption>Friend</TagOption>
+            <TagOption>Family</TagOption>
+            <TagOption>Other</TagOption>
+          </TagInput>
+        </TagLabel>
+
+        <LastTalkedLabel>
+          Last Time We Have Spoken
           <DatePickerComponent
             setStartDate={setStartDate}
             startDate={startDate}
           />
-          <AddSubmitInput
-            disabled={name === ""}
-            type="submit"
-            value="Add contact"
+        </LastTalkedLabel>
+
+        <NotesLabel>
+          Add a note
+          <NotesInput
+            value={note}
+            onChange={(e) => {
+              setNote(e.target.value);
+            }}
           />
-        </DatePickerWrapper>
+        </NotesLabel>
+
+        <AddSubmitInput
+          disabled={name === ""}
+          type="submit"
+          value="Add contact"
+        />
       </AddContactForm>
       {error && <ErrorWrapper errorMessage={error} />}
     </>
