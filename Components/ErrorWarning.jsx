d3d6e127480aed11e74920e9c7ff0c531ac8bb2e
Ido Mandelman <idomand@gmail.com>
2022-02-21 11:30:34 +0100

    better Error massage

--- Components/ErrorWarning.jsx
+++ Components/ErrorWarning.jsx
@@ -1,17 +1,47 @@
 import React from "react";
 import propTypes from "prop-types";
+import styled, { keyframes } from "styled-components";
+import { H5 } from "./Common/StyledText";
 
-import styled from "styled-components";
-import { H1 } from "./Common/StyledText";
+const slide = keyframes`
+  from {
+    transform: translateY(0);
+    opacity: 0
+  }
+  to {
+    transform: translateY(40px);
+    opacity: 1
+  }
+`;
 
-const ErrorWrapper = styled.div``;
+const ErrorWrapper = styled.div`
+  border: solid ${({ theme }) => theme.red1};
+  background-color: ${({ theme }) => theme.red2};
+  box-shadow: 0px 4px 28px rgba(0, 0, 0, 0.25);
+  border-radius: 13px;
+  padding: 15px 20px;
+  position: absolute;
+  animation: ${slide} 300ms forwards;
+`;
 
-const ErrorText = styled(H1)``;
+const ErrorLogo = styled.img`
+  margin-right: 10px;
+`;
+
+const ErrorText = styled(H5)`
+  color: ${({ theme }) => theme.red1};
+  display: flex;
+  align-items: center;
+  text-transform: capitalize;
+`;
 
 export default function ErrorWarning({ errorMessage }) {
   return (
     <ErrorWrapper>
-      <ErrorText> ⛔ {errorMessage}</ErrorText>
+      <ErrorText>
+        <ErrorLogo src="/Error.svg" />
+        {errorMessage}
+      </ErrorText>
     </ErrorWrapper>
   );
 }
@@ -19,3 +49,42 @@ export default function ErrorWarning({ errorMessage }) {
 ErrorWarning.propTypes = {
   errorMessage: propTypes.string,
 };
+
+// import React from "react";
+// import propTypes from "prop-types";
+// import styled from "styled-components";
+// import { H5 } from "./Common/StyledText";
+
+// const ErrorWrapper = styled.div`
+//   border: solid ${({ theme }) => theme.red1};
+//   background-color: ${({ theme }) => theme.red2};
+//   box-shadow: 0px 4px 28px rgba(0, 0, 0, 0.25);
+//   border-radius: 13px;
+//   padding: 15px 20px;
+// `;
+
+// const ErrorLogo = styled.img`
+//   margin-right: 10px;
+// `;
+
+// const ErrorText = styled(H5)`
+//   color: ${({ theme }) => theme.red1};
+//   display: flex;
+//   align-items: center;
+//   text-transform: capitalize;
+// `;
+
+// export default function ErrorWarning({ errorMessage }) {
+//   return (
+//     <ErrorWrapper>
+//       <ErrorText>
+//         <ErrorLogo src="/Error.svg" />
+//         {errorMessage}
+//       </ErrorText>
+//     </ErrorWrapper>
+//   );
+// }
+
+// ErrorWarning.propTypes = {
+//   errorMessage: propTypes.string,
+// };
