9645652132541fea12da8cb80a0f32287ae74132
Ido Mandelman <idomand@gmail.com>
2025-02-01 14:23:27 +0100

    Update TypeScript reference link and refactor PWA configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
-const withPWA = require("next-pwa");
+// const withPWA = require("next-pwa");
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//     // disable: process.env.NODE_ENV === 'development'
+//   },
+// });
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  // Uncomment the following line if you want to disable PWA in development
+  // disable: process.env.NODE_ENV === 'development'
+});
 
 module.exports = withPWA({
   reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    // disable: process.env.NODE_ENV === 'development'
-  },
 });
