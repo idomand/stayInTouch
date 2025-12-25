21bc9f4d53cb7d2586713d44078be5dfc5733b7e
Ido Mandelman <idomand@gmail.com>
2025-12-25 18:28:39 +0100

    Fix rendering condition for AddNewContact component in MainForm

--- Components/MainForm/index.tsx
+++ Components/MainForm/index.tsx
@@ -36,8 +36,8 @@ export default function MainForm() {
             Make a friend
           </MakeAFriend>
         </div>
-        {showMainForm && <AddNewContact />}
       </MainFormWrapper>
+      {showMainForm && <AddNewContact />}
     </>
   );
 }
